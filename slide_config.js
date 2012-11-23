62e39142acef387af54da0fd7364abd884bf8f4b
Aidan Delaney <aidan@phoric.eu>
2012-11-23 01:16:14 +0000

    Inital CoHub talk

--- slide_config.js
+++ slide_config.js
@@ -1,36 +1,31 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'Shipping Software with Fewer Defects',
+    subtitle: '',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/BrightonUniTiny.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
-      'Source Code Pro'
+      'Inconsolata'
     ],
     //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
   },
 
   // Author information
-  presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
-  }/*, {
-    name: 'Second Name',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
-  }*/]
+  presenters: [
+  {
+    name: '<strong>Dr Aidan Delaney</strong>',
+    email: 'a.j.delaney@brighton.ac.uk',
+    company: 'Visual Modelling Group, University of Brighton',
+//    gplus: 'https://plus.google.com/102275089060106683788',
+    twitter: '@aidandelaney',
+    www: 'http://www.it.brighton.ac.uk/research/vmg/',
+    github: 'https://github.com/AidanDelaney'
+  }
+]
 };
-
